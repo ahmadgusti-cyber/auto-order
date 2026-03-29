eb5abdeba1f704d673ca526837a6d265f522b8fb
ahmadgustii <ahmdgustii@gmail.com>
2026-03-29 08:16:27 +0800

    fix double server

--- web.py
+++ web.py
@@ -215,7 +215,7 @@ def dashboard():
 
     return render_template_string(html)
 
-import os
-
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
